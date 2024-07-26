a92f2b79e0c8fbf686445714584d0f2f0c501116
tanishq <sejwartanishq9691@gmail.com>
2024-07-26 12:57:51 +0530

    first commit

--- program1.js
+++ program1.js
@@ -1,8 +1,36 @@
 const getTotalIsles = function (grid) {
+  if (!grid || grid.length === 0) {
+    return 0;
+  }
 
+  const numRows = grid.length;
+  const numCols = grid[0].length;
+  let islandCount = 0;
 
-  // write your code here
+  const dfs = (i, j) => {
+    if (i < 0 || i >= numRows || j < 0 || j >= numCols || grid[i][j] === '0') {
+      return;
+    }
 
+    grid[i][j] = '0'; 
+
+    
+    dfs(i + 1, j);
+    dfs(i - 1, j);
+    dfs(i, j + 1);
+    dfs(i, j - 1);
+  };
+
+  for (let i = 0; i < numRows; i++) {
+    for (let j = 0; j < numCols; j++) {
+      if (grid[i][j] === '1') {
+        islandCount++;
+        dfs(i, j); 
+      }
+    }
+  }
+
+  return islandCount;
 };
 
 module.exports = getTotalIsles;
--- program2.js
+++ program2.js
@@ -1,7 +1,36 @@
 const decodeTheRing = function (s, p) {
-
-    // write your code here
-
-  };
+  const m = s.length;
+  const n = p.length;
   
-  module.exports = decodeTheRing;
+  const dp = Array(n + 1).fill(false).map(() => Array(m + 1).fill(false));
+  
+  
+  dp[0][0] = true;
+  
+  
+  for (let i = 1; i <= n; i++) {
+      if (p[i - 1] === '*') {
+          dp[i][0] = dp[i - 1][0];
+      }
+  }
+  
+
+  for (let i = 1; i <= n; i++) {
+      for (let j = 1; j <= m; j++) {
+          if (p[i - 1] === '*') {
+              
+              dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+          } else if (p[i - 1] === '?' || p[i - 1] === s[j - 1]) {
+
+              dp[i][j] = dp[i - 1][j - 1];
+          } else {
+              dp[i][j] = false;
+          }
+      }
+  }
+  
+  
+  return dp[n][m];
+};
+
+module.exports = decodeTheRing;
